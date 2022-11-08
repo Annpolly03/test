0804b08ccb617acf8f71d3009629359dfb13dd7c
Annpolly03 <108858713+Annpolly03@users.noreply.github.com>
2022-11-09 00:45:56 +0530

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -4,7 +4,7 @@ int main()
     int i,n;
     int t1=0,t2=1;
     int nextTerm = t1+t2;
-    printf("enter the number of terms:");
+    printf("enter the num terms:");
     scanf("%d",&n);
     printf("fibonacci series:%d,%d,",t1,t2);
 
